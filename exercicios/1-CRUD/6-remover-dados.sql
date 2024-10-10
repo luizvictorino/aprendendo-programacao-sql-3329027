37f9239fa17eb76e9b6a81a41a5979d8bd979d6b
luizvictorino <luiz.apvictorino@gmail.com>
2024-10-09 22:15:35 -0300

    Praticando SQL exercicios

--- exercicios/1-CRUD/6-remover-dados.sql
+++ exercicios/1-CRUD/6-remover-dados.sql
@@ -1,4 +1,9 @@
 -- Da tabela invoice, remova o invoice cujo o InvoiceID corresponda ao valor 33
+SELECT * FROM invoices
 
+DELETE FROM invoices
+WHERE InvoiceId = 33;
 
 -- Da tabela invoice, remova todos os invoices cujo o Total seja menor do que 1
+DELETE FROM invoices
+WHERE Total < 1;
--- exercicios/2-construindo-consultas/1-operadores.sql
+++ exercicios/2-construindo-consultas/1-operadores.sql
@@ -1,6 +1,26 @@
 -- Construa uma consulta para a tabela "invoice_items" que retorne apenas as colunas InvoiceId, TrackId e UnitPrice.
+SELECT * FROM invoice_items
+
+SELECT InvoiceId, TrackId, UnitPrice
+FROM invoice_items;
 
 -- Inclua nessa consulta mais três colunas, cada uma como resultado de uma operação aritmética diferente
+SELECT 
+  InvoiceLineId, 
+  TrackId, 
+  UnitPrice, 
+  UnitPrice * (Quantity + 1),
+  Quantity + 3
+FROM invoice_items
+
+WHERE InvoiceId < 4;
 
 -- Incremente essa consulta utilizando a cláusula WHERE, a qual restringirá o resultado apenas para linhas cujo o InvoiceId seja maior do que 20 e menor ou igual a 30
+SELECT 
+  InvoiceLineId,
+  InvoiceId,
+  TrackId,
+  UnitPrice
+FROM invoice_items
+WHERE InvoiceLineId > 20 AND InvoiceLineId <= 30
 
